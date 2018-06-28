78e2f23e1f5a79b7f4d9686bb7c050db7bb07f58
айкеновад <@234_11>
2018-06-28 08:39:04 +0600

    Add project files.

---
+++ 28.06.2018/Source.cpp
@@ -0,0 +1,80 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<math.h>
+#include<time.h>
+#include <iostream>
+#include <iomanip> 
+
+using namespace std;
+
+void main()
+{
+	srand(time(NULL));
+
+	setlocale(LC_ALL, "Rus");
+
+	int tn = 0;
+
+start:
+
+	printf("task number:\t");
+	scanf("%d", &tn);
+
+	switch (tn)
+	{
+	case 1:
+	{
+		
+	}
+	break;
+	case 2:
+	{
+
+	}
+	break;
+	case 3:
+	{
+
+	}
+	break;
+	case 4:
+	{
+
+	}
+	break;
+	case 5:
+	{
+
+	}
+	break;
+	case 6:
+	{
+
+	}
+	break;
+	case 7:
+	{
+
+	}
+	break;
+	case 8:
+	{
+
+	}
+	break;
+	case 9:
+	{
+
+	}
+	break;
+	case 10:
+	{
+
+	}
+	break;
+	default:
+		printf("\nerror: no tasks\n\n");
+	}
+	goto start;
+}
